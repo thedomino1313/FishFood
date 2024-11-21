4feac78a41ee2f308d14f4b96ceca996cb9cee78
thedomino1313 <thedomino1313@DomLaptop.lan>
2024-11-21 18:24:25 -0500

    added files required locally

--- WF_SDK/tools.py
+++ WF_SDK/tools.py
@@ -6,22 +6,10 @@ from os import sep                # OS specific file path separators
 from math import log10, sqrt      # import necessary math functions
 
 # load the dynamic library, get constants path (the path is OS specific)
-if platform.startswith("win"):
-    # on Windows
-    dwf = ctypes.cdll.dwf
-    constants_path = "C:" + sep + "Program Files (x86)" + sep + "Digilent" + sep + "WaveFormsSDK" + sep + "samples" + sep + "py"
-elif platform.startswith("darwin"):
-    # on macOS
-    lib_path = sep + "Library" + sep + "Frameworks" + sep + "dwf.framework" + sep + "dwf"
-    dwf = ctypes.cdll.LoadLibrary(lib_path)
-    constants_path = sep + "Applications" + sep + "WaveForms.app" + sep + "Contents" + sep + "Resources" + sep + "SDK" + sep + "samples" + sep + "py"
-else:
-    # on Linux
-    dwf = ctypes.cdll.LoadLibrary("libdwf.so")
-    constants_path = sep + "usr" + sep + "share" + sep + "digilent" + sep + "waveforms" + sep + "samples" + sep + "py"
+dwf = ctypes.CDLL("./libdwf.so")
 
-# import constants
-path.append(constants_path)
+
+
 import dwfconstants as constants
 
 """-----------------------------------------------------------------------"""
